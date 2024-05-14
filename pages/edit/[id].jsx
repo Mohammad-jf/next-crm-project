251ce222dfad089dd23f27f657e3e44736e61099
mohammad jalifathi <m.h.jalifathi@gmail.com>
2024-05-15 02:37:08 +0330

    some fixes

--- pages/edit/[id].jsx
+++ pages/edit/[id].jsx
@@ -3,12 +3,15 @@ import { useRouter } from 'next/router'
 
 const Customer = () => {
     const router = useRouter();
-    const { id } = router.query
-    return (
-        <div>
-            <EditCustomerPage id={id} />
-        </div>
-    )
+    const { query: { id }, isReady } = router
+
+    if (isReady) {
+        return (
+            <div>
+                <EditCustomerPage id={id} />
+            </div>
+        )
+    }
 }
 
 export default Customer
